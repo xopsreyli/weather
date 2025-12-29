2ab1415e9253c0d43981de6eca9b057e65ac7b64
xopsreyli <xopsreyli@gmail.com>
2025-12-30 01:05:14 +0200

    update

--- src/pages/Weather/Location/Location.tsx
+++ src/pages/Weather/Location/Location.tsx
@@ -47,7 +47,7 @@ const Location = () => {
                         <p className={styles.condition__text}>{current.condition.text}</p>
                         <div className={styles['condition__min-max']}>
                             <div className={styles['min-max']}>
-                                <Arrow customClass={[styles.arrow, styles['arrow--down']].join(' ')} />
+                                <Arrow customClass={[styles['min-max__arrow'], styles['min-max__arrow--down']].join(' ')} />
                                 <Temperature
                                     tempClass={styles['min-max__temperature']}
                                     temperature={Math.round(forecast.forecastday[0].day.mintemp_c)}
@@ -59,7 +59,7 @@ const Location = () => {
                                     tempClass={styles['min-max__temperature']}
                                     temperature={Math.round(forecast.forecastday[0].day.maxtemp_c)}
                                 />
-                                <Arrow customClass={styles.arrow} />
+                                <Arrow customClass={styles['min-max__arrow']} />
                             </div>
                         </div>
                     </div>
